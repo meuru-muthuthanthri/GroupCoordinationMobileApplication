ab1646b67475250851aa1dd312989fa7b580de2c
meuru <meuru@live.com>
2013-09-11 09:13:44 +0530

    Message sending is working perfectly

--- GCMAClient/src/com/cse/gcmaclient/MainActivity.java
+++ GCMAClient/src/com/cse/gcmaclient/MainActivity.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View.OnClickListener;
@@ -25,36 +26,8 @@ public class MainActivity extends Activity {
 	Button sendButton;
 
 	static final int BUFFER_SIZE = 516;
-	private DatagramSocket communicationSocket;
-	DatagramPacket FinalPacket;
-	// ChatPacket PacketToServer;
-	String MachinePort;
-	String Machine;
-	String Port;
 	String Username = "meuru";
-	String MessageTo;
-	String Message;
-	String serverAddress = "localhost";
-	String Blank = "0";
-	int PacketType;
-	int MessageLength;
-	int UsernameLength;
-	int MessageToLength;
-	int PortInt = 9999;
-	int MachineLength;
-	int PortLength;
-	byte[] FinalBytePacket;
-	byte[] data;
-	byte[] PacketTypeByte = new byte[1];
-	byte[] ULengthByte;
-	byte[] UsernameByte;
-	byte[] MessageToLengthByte;
-	byte[] MessageToByte;
-	byte[] MessageLengthByte;
-	byte[] MessageByte;
-	byte[] ResvByte;
-	byte[] BlankByte = new byte[1];
-	String UIStatus = new String("Not Set Yet\n");
+	static final String Blank = "0";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -70,8 +43,7 @@ public class MainActivity extends Activity {
 		// add click listeners to the buttons
 		sendButton.setOnClickListener(sendButtonListener);
 
-		establishCommunication();
-		// updateCommunicationHistory();
+		new Communicator().execute("I");
 	}
 
 	public OnClickListener sendButtonListener = new OnClickListener() {
@@ -79,7 +51,7 @@ public class MainActivity extends Activity {
 		@Override
 		public void onClick(View v) {
 			if (chatMessageEditText.getText().length() > 0) {
-				sendChatMessage();
+				//sendChatMessage();
 
 				// Update Interface States
 				chatMessageEditText.setText("");
@@ -90,98 +62,6 @@ public class MainActivity extends Activity {
 	};
 
 	// this methods handles all the communication
-	private void establishCommunication() {
-		try {
-			// create the master thread for the new client interface
-			communicationSocket = new DatagramSocket();
-		} catch (SocketException se) {
-			System.err.println(se);
-		}
-
-		// start the message receiver
-
-		initializeUserName();
-	}
-
-	private void initializeUserName() {
-		PacketType = 1;
-		FinalBytePacket = MakePacket(PacketType, Username, Blank);
-		SendPacket(FinalBytePacket);
-
-	}
-
-	public byte[] MakePacket(int PacketType, String Username, String Message) {
-
-		// This method creates a packet from a packet type, user name, and
-		// message
-		// Packet type => 1 = initializing user name
-		// 2 = leave message
-		// 3 = chat message
-
-		data = new byte[512];
-
-		byte[] UsernameA = Username.getBytes();
-		byte[] MessageA = Message.getBytes();
-
-		int i = 0;
-		// TRY MAKING A STRING AND CONVERING TO A BYTE ARRAY
-
-		data[i++] = (byte) PacketType;
-		data[i++] = (byte) 'R';
-		data[i++] = (byte) 'E';
-		data[i++] = (byte) UsernameA.length;
-		for (int n = 0; n < UsernameA.length; n++)
-			data[i++] = UsernameA[n];
-		data[i++] = (byte) MessageA.length;
-		for (int n = 0; n < MessageA.length; n++)
-			data[i++] = MessageA[n];
-
-		return data;
-	}
-
-	public void SendPacket(byte[] SendingBytePacket) {
-
-		// method which sends the packet that was created out to the server
-
-		try {
-			InetAddress serverIP;
-			// serverIP = InetAddress.getByName(serverAddress);
-			serverIP = InetAddress.getByName("10.8.108.200");
-			PortInt = 9999;
-			// System.out.println("Server IP : "+ serverIP.toString());
-			FinalPacket = new DatagramPacket(SendingBytePacket,
-					SendingBytePacket.length, serverIP, PortInt);
-			System.out.println(" Trying to send packet port : "
-					+ FinalPacket.getPort() + " serverIP : "
-					+ FinalPacket.getAddress().toString());
-			System.out.println("datagram port :"
-					+ communicationSocket.getPort());
-			communicationSocket.send(FinalPacket);
-
-		} catch (UnknownHostException e) {
-			System.err.println(e);
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-	}
-
-	private void sendChatMessage() {
-		PacketType = 3;
-		setValues();
-		FinalBytePacket = MakePacket(PacketType, MessageTo, Message);
-		SendPacket(FinalBytePacket);
-	}
-
-	private void sendleaveMessage() {
-		PacketType = 2;
-		FinalBytePacket = MakePacket(PacketType, Blank, Blank);
-		SendPacket(FinalBytePacket);
-	}
-
-	private void setValues() {
-		MessageTo = "";
-		Message = chatMessageEditText.getText().toString();
-	}
 
 	private void updateCommunicationHistory() {
 		// TODO Auto-generated method stub
@@ -193,6 +73,117 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	/**
+	 * Communicator class is responsible for sending messages to the remote
+	 * server.
+	 * 
+	 * @author Meuru Muthuthanthri
+	 * 
+	 */
+	private class Communicator extends AsyncTask<String, String, String> {
+
+		public static final String SERVERIP = "10.100.7.171";
+		public static final int SERVERPORT = 9999;
+
+		private InetAddress serverAddr;
+		private DatagramSocket socket;
+
+		public Communicator() {
+			super();
+			try {
+				serverAddr = InetAddress.getByName(SERVERIP);
+				socket = new DatagramSocket();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		protected String doInBackground(String... args) {
+			try {
+				initializeUser();
+
+				byte[] buf;
+				// if (!input.getText().toString().isEmpty()) {
+				// buf = input.getText().toString().getBytes();
+				// } else {
+				buf = ("A new message frddddddom meuru").getBytes();
+				// }
+				sendPacket(buf);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		/**
+		 * Sends a UDP packet using the datagram socket
+		 * 
+		 * @param packetMsg
+		 *            The message required to send
+		 * @throws IOException
+		 */
+		private void sendPacket(byte[] packetMsg) throws IOException {
+			DatagramPacket packet = new DatagramPacket(packetMsg,
+					packetMsg.length, serverAddr, SERVERPORT);
+			socket.send(packet);
+		}
+
+		private byte[] makePacket(int PacketType, String Username,
+				String Message) {
+
+			// This method creates a packet from a packet type, user name, and
+			// message
+			// Packet type => 1 = initializing user name
+			// 2 = leave message
+			// 3 = chat message
+
+			byte[] data = new byte[512];
+
+			byte[] UsernameA = Username.getBytes();
+			byte[] MessageA = Message.getBytes();
+
+			int i = 0;
+			// TRY MAKING A STRING AND CONVERING TO A BYTE ARRAY
+
+			data[i++] = (byte) PacketType;
+			data[i++] = (byte) 'R';
+			data[i++] = (byte) 'E';
+			data[i++] = (byte) UsernameA.length;
+			for (int n = 0; n < UsernameA.length; n++)
+				data[i++] = UsernameA[n];
+			data[i++] = (byte) MessageA.length;
+			for (int n = 0; n < MessageA.length; n++)
+				data[i++] = MessageA[n];
+
+			return data;
+		}
+
+		private void initializeUser() throws IOException {
+			int packetType = 1;
+			byte[] bytePacket = makePacket(packetType, Username, Blank);
+			sendPacket(bytePacket);
+		}
+
+		private void sendChatMessage() throws IOException {
+			int packetType = 3;
+			String messageTo = "";
+			String message = chatMessageEditText.getText().toString();
+			byte[] bytePacket = makePacket(packetType, messageTo, message);
+			sendPacket(bytePacket);
+		}
+
+		private void sendleaveMessage() throws IOException {
+			int packetType = 2;
+			byte[] bytePacket = makePacket(packetType, Blank, Blank);
+			sendPacket(bytePacket);
+		}
+
+		
 	}
 
 }
